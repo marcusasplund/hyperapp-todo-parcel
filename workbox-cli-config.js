e6abe42837bd306827a2e09854ae6f4999dc070f
Marcus Asplund <rootfood@gmail.com>
2018-01-15 20:16:40 +0100

    Update workbox-cli-config.js

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   'globDirectory': 'dist/',
-  'globPatterns': [
-    '**/*.{png,css,js,woff,svg,xml,ico,woff2,html,json}'
-  ],
+  'globPatterns': [],
   'swDest': 'build/sw.js',
   'globIgnores': [
     '../workbox-cli-config.js'
