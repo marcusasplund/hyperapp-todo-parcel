607b5fb6395b09818700370bfe9753aad949fce5
Marcus Asplund <marasp@chamarap2018MBP.lan>
2018-04-04 19:34:10 +0200

    workbox update

--- workbox-config.js
+++ workbox-config.js
@@ -1,18 +1,7 @@
 module.exports = {
-  'globDirectory': 'somedir/',
+  'globDirectory': 'dist/',
   'swDest': 'build/sw.js',
   'navigateFallback': 'dist/index.html',
   'clientsClaim': true,
-  'skipWaiting': true,
-  'runtimeCaching': [
-    {
-      'urlPattern': 'https://cdn.rawgit.com/parcel-bundler/website/01a1f7dd/src/assets/parcel%403x.png',
-      'handler': 'cacheFirst',
-      'options': {
-        'cacheableResponse': {
-          'statuses': [0]
-        }
-      }
-    }
-  ]
+  'skipWaiting': true
 }
